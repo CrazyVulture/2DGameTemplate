ce6ba840a48986d9e015a6887f63e5746872dcd6
CrazyVulture <gaochen010@sina.com>
2019-01-31 17:53:04 +0800

    add more items and effects

---
+++ Assets/Scripts/Effects/Explode.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour
+{
+    public GameObject explosion;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Instantiate(explosion, transform.position, Quaternion.identity);
+            gameObject.SetActive(false);
+        }
+    }
+}
